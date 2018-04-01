8032d8016eb293e66826b87121403d3167b404d5
Michel-Ange <michelange@partikule.net>
2018-04-01 20:04:47 +0200

    Lang file, dedicated to the media manager

---
+++ resources/lang/en/mediamanager.php
@@ -0,0 +1,87 @@
+<?php
+
+return [
+
+    'name' => 'Media Manager',
+    'description' => 'One great Vue.js / Bulma Media Manager',
+
+    'text' => [
+        'empty_folder' => 'This folder is empty.',
+        'drag_n_drop' => 'Drag and drop files onto this window to upload files.',
+        'upload_finished' => 'Upload process finished',
+    ],
+
+    'confirmation' => [
+        'sure' => 'Are your sure?',
+        'delete' => 'Delete this item?',
+        'delete_multiple' => 'Delete all these items?',
+        'move' => 'Move this item?',
+        'move_multiple' => 'Move all these items?',
+    ],
+
+    'label' => [
+        'move_to' => 'Move to',
+        'rename_to' => 'Rename to',
+        'name' => 'Name',
+        'type' => 'Type',
+        'date' => 'Date',
+        'size' => 'Size',
+        'url' => 'URL',
+        'uploaded_on' => 'Uploaded On',
+        'folder_name' => 'Folder Name',
+        'new_name' => 'New Name',
+    ],
+
+    'button' => [
+
+        'cancel' => [
+            'title' => 'Cancel',
+            'label' => 'Cancel',
+        ],
+        'select' => [
+            'title' => 'Select',
+            'label' => 'Select',
+        ],
+        'select_close' => [
+            'title' => 'Select and Close',
+            'label' => 'Select and Close',
+        ],
+        'refresh' => [
+            'title' => 'Refresh',
+            'label' => 'Refresh',
+        ],
+        'delete' => [
+            'title' => 'Delete',
+            'label' => 'Delete',
+        ],
+        'delete_multiple' => [
+            'title' => 'Delete items',
+            'label' => 'Delete items',
+        ],
+        'move' => [
+            'title' => 'Move item to another folder',
+            'label' => 'Move',
+        ],
+        'rename' => [
+            'title' => 'Rename',
+            'label' => 'Rename',
+        ],
+        'add_folder' => [
+            'label' => 'Add folder',
+            'title' => 'Create a new folder in the current folder',
+        ],
+        'create_folder' => [
+            'label' => 'Create folder',
+            'title' => 'Create folder',
+        ],
+        'upload' => [
+            'title' => 'Select files to be uploaded - or drag files into the main window panel',
+            'label' => 'Upload',
+        ],
+        'file_url' => [
+            'title' => 'Open in new Browser Tab',
+            'label' => 'Open',
+        ],
+    ],
+
+];
